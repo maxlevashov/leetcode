1fbd50729e82759cdd7d120f254a29726290bf5f
bgdan83 <maklev@mail.ru>
2022-05-26 14:26:02 +0300

    1143. Longest Common Subsequence

---
+++ php/1143_Longest_Common_Subsequence.php
@@ -0,0 +1,34 @@
+<?php
+class Solution {
+
+    /**
+     * @param String $text1
+     * @param String $text2
+     * @return Integer
+     */
+    function longestCommonSubsequence($text1, $text2) {
+        $memo = [];
+        $textFirstStrlen = strlen($text1) + 1;
+        $textTwoStrlen = strlen($text2) + 1;
+        for ($i = 0; $i < $textFirstStrlen; $i++) {
+            for ($j = 0; $j < $textTwoStrlen; $j++) {
+                $memo[$i][$j] = 0;
+            }
+        }
+        $maxLength = 0;
+        for ($i = 1; $i < $textFirstStrlen; $i++) {
+            for ($j = 1; $j < $textTwoStrlen; $j++) {
+                if ($text1[$i - 1] == $text2[$j - 1]) {
+                    $memo[$i][$j] = 1 + $memo[$i - 1][$j - 1];
+                } else {
+                    $memo[$i][$j] = max($memo[$i - 1][$j], $memo[$i][$j - 1]);
+                }
+
+                $maxLength = max($maxLength, $memo[$i][$j]);
+            }
+        }
+        
+        return $maxLength;
+    }
+
+}
