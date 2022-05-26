54b8706aef9abe6ae8cd24d108b32ea1833575dd
bgdan83 <maklev@mail.ru>
2022-05-26 16:46:24 +0300

    add short write

--- php/1143_Longest_Common_Subsequence.php
+++ php/1143_Longest_Common_Subsequence.php
@@ -10,11 +10,15 @@ class Solution {
         $memo = [];
         $textFirstStrlen = strlen($text1) + 1;
         $textTwoStrlen = strlen($text2) + 1;
+
+        // short write memo2 = array_fill(0, $textFirstStrlen, array_fill(0, $textTwoStrlen, 0));
+        // this for multilanguage
         for ($i = 0; $i < $textFirstStrlen; $i++) {
             for ($j = 0; $j < $textTwoStrlen; $j++) {
                 $memo[$i][$j] = 0;
             }
         }
+
         $maxLength = 0;
         for ($i = 1; $i < $textFirstStrlen; $i++) {
             for ($j = 1; $j < $textTwoStrlen; $j++) {
