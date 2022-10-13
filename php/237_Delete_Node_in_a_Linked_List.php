d5e8210ebc08626611c4a7cd6750caec25dbfc4b
bgdan83 <maklev@mail.ru>
2022-10-13 07:37:50 +0300

    237. Delete Node in a Linked List

---
+++ php/237_Delete_Node_in_a_Linked_List.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val) { $this->val = $val; }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param ListNode $node
+     * @return 
+     */
+    function deleteNode($node) {
+        $nextNode = $node->next;
+        $node->val = $nextNode->val;
+        $node->next = $nextNode->next;
+        unset($nextNode);
+    }
+}
