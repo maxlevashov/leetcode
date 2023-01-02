31ad1586d2b4af2f73ad268e1ecb0d01e38de0b9
bgdan83 <maklev@mail.ru>
2023-01-02 10:29:24 +0300

    1. Two Sum - js

---
+++ js/1_Two_Sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    numsSwap = {};
+    result = [-1, -1];
+    nums.forEach((element, index) => {
+        if (numsSwap.hasOwnProperty(target - element)) {
+            result = [numsSwap[target - element], index];
+        } else {
+            numsSwap[element] = index;
+        }
+    });
+    return result;
+};
