af1a5af69c9db0c96c561890397ab89348c18614
bgdan83 <maklev@mail.ru>
2023-01-15 14:31:05 +0300

    141. Linked List Cycle

---
+++ php/141_Linked_List_Cycle.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val) { $this->val = $val; }
+ * }
+ */
+class Solution
+{
+
+    /**
+     * @param ListNode $head
+     * @return Boolean
+     */
+    function hasCycle($head)
+    {
+        if ($head == null) {
+            return false;
+        }
+
+        $slow = $head;
+        $fast = $head->next;
+        while ($slow !== $fast) {
+            if ($fast === null || $fast->next === null) {
+                return false;
+            }
+            $slow = $slow->next;
+            $fast = $fast->next->next;
+        }
+
+        return true;
+    }
+
+}
