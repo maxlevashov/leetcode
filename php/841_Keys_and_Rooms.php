f848ee12cd02461891e90e13184c2b14835be541
bgdan83 <maklev@mail.ru>
2023-01-15 17:24:08 +0300

    841. Keys and Rooms

---
+++ php/841_Keys_and_Rooms.php
@@ -0,0 +1,35 @@
+<?php
+
+class Solution
+{
+
+    /**
+     * @param Integer[][] $rooms
+     * @return Boolean
+     */
+    function canVisitAllRooms($rooms)
+    {
+        $arVisited = array_fill(0, count($rooms), false);
+        $arVisited[0] = true;
+        $arKeys[] = 0;
+
+        while (!empty($arKeys)) {
+            $currentKey = array_pop($arKeys);
+            foreach ($rooms[$currentKey] as $key) {
+                var_dump($key);
+                if (!$arVisited[$key]) {
+                    $arVisited[$key] = true;
+                    $arKeys[] = $key;
+                }
+            }
+        }
+        foreach ($arVisited as $visitedRoom) {
+            if (!$visitedRoom) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
