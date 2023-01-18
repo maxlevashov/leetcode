f6af41771cc182f6ca7c34848843d3ac96ae7071
bgdan83 <maklev@mail.ru>
2023-01-18 15:56:29 +0300

    917. Reverse Only Letters - single pass version

--- php/917_Reverse_Only_Letters.php
+++ php/917_Reverse_Only_Letters.php
@@ -9,6 +9,26 @@ class Solution
      */
     function reverseOnlyLetters($s)
     {
+        
+//        single pass version
+//        
+//        $result = '';
+//        $stringLength = strlen($s);
+//        $j = $stringLength - 1;
+//
+//        for ($i = 0; $i < $stringLength; $i++) {
+//            if (ctype_alpha($s[$i])) {
+//                while (!ctype_alpha($s[$j])) {
+//                    $j--;
+//                }
+//                $result .= $s[$j];
+//                $j--;
+//            } else {
+//                $result .= $s[$i];
+//            }
+//        }
+        
+        
         $result = '';
         $stringLength = strlen($s);
         $stack = [];
