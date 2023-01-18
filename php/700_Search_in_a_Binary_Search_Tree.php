a3a0d43fb3a5b991db39c533dba3e31871b4e9bc
bgdan83 <maklev@mail.ru>
2023-01-18 13:27:59 +0300

    700. Search in a Binary Search Tree

---
+++ php/700_Search_in_a_Binary_Search_Tree.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution
+{
+
+    /**
+     * @param TreeNode $root
+     * @param Integer $val
+     * @return TreeNode
+     */
+    function searchBST($root, $val)
+    {
+        if ($root === null || $root->val == $val) {
+            return $root;
+        }
+        if ($val < $root->val) {
+            return $this->searchBST($root->left, $val);
+        } else {
+            return $this->searchBST($root->right, $val);
+        }
+    }
+
+}
