0aa9e4e611356462b2cdd56d3c894ec540b71088
bgdan83 <maklev@mail.ru>
2023-01-28 08:27:16 +0300

    13. Roman to Integer

---
+++ java/13_Roman_to_Integer.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> numbers = new HashMap<Character, Integer>();
+        numbers.put('I', 1);
+        numbers.put('V', 5);
+        numbers.put('X', 10);
+        numbers.put('L', 50);
+        numbers.put('C', 100);
+        numbers.put('D', 500);
+        numbers.put('M', 1000);
+        int value = 0;
+        for (int i = 0; i < s.length();  i++) {
+            if (i + 1 < s.length() && numbers.get(s.toCharArray()[i]) < numbers.get(s.toCharArray()[i + 1])) {
+                value += numbers.get(s.toCharArray()[i + 1]) - numbers.get(s.toCharArray()[i]);
+                i++;
+            } else {
+                value += numbers.get(s.toCharArray()[i]);
+            }
+        }
+
+        return value;
+    }
+}
+
