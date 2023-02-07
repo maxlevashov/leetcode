ed2c34f822a5ad9e8044c648e049a979e65dabad
bgdan83 <maklev@mail.ru>
2023-02-07 09:50:55 +0300

    26. Remove Duplicates from Sorted Array

---
+++ php/904_Fruit_Into_Baskets.php
@@ -0,0 +1,31 @@
+<?php
+
+class Solution
+{
+
+    /**
+     * @param Integer[] $fruits
+     * @return Integer
+     */
+    function totalFruit($fruits)
+    {
+
+        $basket = [];
+        $left = 0;
+        $right = 0;
+
+        for (; $right < count($fruits); ++$right) {
+            $basket[$fruits[$right]]++;
+            if (count($basket) > 2) {
+                $basket[$fruits[$left]]--;
+                if ($basket[$fruits[$left]] == 0) {
+                    unset($basket[$fruits[$left]]);
+                }
+                $left++;
+            }
+        }
+
+        return $right - $left;
+    }
+
+}
