4a33089a389fa012936abea54776556370fca234
bgdan83 <maklev@mail.ru>
2023-02-26 11:45:53 +0300

    1929. Concatenation of Array

---
+++ php/1929_Concatenation_of_Array.php
@@ -0,0 +1,12 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer[]
+     */
+    function getConcatenation($nums) {
+        return array_merge($nums, $nums);
+    }
+}
