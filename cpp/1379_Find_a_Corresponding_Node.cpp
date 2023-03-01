bb50147b78f3f1698e8297e606e46f88aad95be1
bgdan83 <maklev@mail.ru>
2023-03-01 11:08:08 +0300

    1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree

--- cpp/1379_Find_a_Corresponding_Node.cpp
+++ cpp/1379_Find_a_Corresponding_Node.cpp
@@ -14,23 +14,22 @@ public:
         return dfs(cloned, target->val);
     }
 
-    TreeNode* dfs(TreeNode* current, int value){
-			if (!current) {
-                return NULL;
-            }
-                       
-			if (current->val == value) {
-                return current;
-            }
-			TreeNode* left = dfs(current->left, value);
-			if (left) {
-                return left;
-            }
-			TreeNode* right = dfs(current->right, value);
-			if (right) {
-                return right;
-            }
-			return NULL;
-		}
-};
+    TreeNode* dfs(TreeNode* current, int value) {
+	if (!current) {
+            return NULL;
+        }
 
+                    if (current->val == value) {
+            return current;
+        }
+                    TreeNode* left = dfs(current->left, value);
+                    if (left) {
+            return left;
+        }
+                    TreeNode* right = dfs(current->right, value);
+                    if (right) {
+            return right;
+        }
+	return NULL;
+	}
+};
