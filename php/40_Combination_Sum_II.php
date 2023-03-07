a362d0ed81b86067ee518186c43e8eea38bdf1cc
bgdan83 <maklev@mail.ru>
2023-03-07 15:44:00 +0300

    40_Combination_Sum_II

---
+++ php/40_Combination_Sum_II.php
@@ -0,0 +1,47 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $candidates
+     * @param Integer $target
+     * @return Integer[][]
+     */
+    function combinationSum2($candidates, $target) {
+        $result = [];
+        $currentComb = [];
+        sort($candidates);
+        $this->combination(0, $target, $candidates, $result, $currentComb);
+        return $result;
+    }
+
+    protected function combination(
+        $i, 
+        $target,
+        &$candidates,
+        &$result,
+        &$currentComb
+    ){
+        if ($target == 0){
+            $result[] = $currentComb;
+            return;
+        }
+        for ($index = $i; $index < count($candidates); $index++){
+            if (
+                $index > $i 
+                && $candidates[$index] == $candidates[$index - 1]
+            ) {
+                continue;
+            }
+            if ($candidates[$index] > $target) {
+                break;
+            }
+            $currentComb[] = $candidates[$index];
+            $this->combination(
+                $index + 1, $target - $candidates[$index], 
+                $candidates, $result, $currentComb);
+            array_pop($currentComb);
+        }
+    }
+}
+
