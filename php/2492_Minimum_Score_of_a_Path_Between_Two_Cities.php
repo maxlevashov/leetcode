42550e9e51a9cd3bd1021debad031b06f20eea87
bgdan83 <maklev@mail.ru>
2023-03-22 10:33:29 +0300

    2492. Minimum Score of a Path Between Two Cities

--- php/2492_Minimum_Score_of_a_Path_Between_Two_Cities.php
+++ php/2492_Minimum_Score_of_a_Path_Between_Two_Cities.php
@@ -1,35 +1,51 @@
 <?php
 
-class Solution
-{
+class Solution {
+
+    protected $graph = [];
+    protected $minScore = 0;
+    protected $visited = [];
+    protected $queue = null;
 
     /**
      * @param Integer $n
      * @param Integer[][] $roads
      * @return Integer
      */
-    function minScore($n, $roads)
-    {
-        $graph = [];
-        foreach ($roads as $road) {
-            $graph[$road[0]][$road[1]] = $graph[$road[1]][$road[0]] = $road[2];
-        }
-        $minScore = PHP_INT_MAX;
-        $visited = [];
-        $queue = new SplQueue();
-        $queue->enqueue(1);
+    function minScore($n, $roads) {
+        
+        $this->init($roads);
+        
+        $this->calculateMinScore();
 
-        while (!$queue->isEmpty()) {
-            $node = $queue->dequeue();
-            foreach ($graph[$node] as $adj => $score) {
-                if (!isset($visited[$adj])) {
-                    $queue->enqueue($adj);
-                    $visited[$adj] = false;
-                }
-                $minScore = min($minScore, $score);
-            }
-        }
-        return $minScore;
+        return $this->minScore;
     }
 
+    protected function init($roads) {
+        foreach ($roads as $road) {
+            $vertex1 = $road[0];
+            $vertex2 = $road[1];
+            $weight = $road[2];
+            $this->graph[$vertex1][$vertex2] = $weight;
+            $this->graph[$vertex2][$vertex1] = $weight;
+        }
+        $this->minScore = PHP_INT_MAX;
+        $this->visited = [];
+        $this->queue = new SplQueue();
+        $this->queue->enqueue(1);
+    }
+
+    protected function calculateMinScore() {
+        while (!$this->queue->isEmpty()) {
+            $node = $this->queue->dequeue();
+            foreach ($this->graph[$node] as $adj => $score) {
+                if (!isset($this->visited[$adj])) {
+                    $this->queue->enqueue($adj);
+                    $this->visited[$adj] = false;
+                }
+                $this->minScore = min($this->minScore, $score);          
+            }
+        }
+    }
+    
 }
