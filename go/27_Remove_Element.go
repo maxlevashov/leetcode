a805c3424ecd6ac029e58b03ed37bd17755e1980
bgdan83 <maklev@mail.ru>
2023-03-26 15:42:26 +0300

    27. Remove Element

---
+++ go/27_Remove_Element.go
@@ -0,0 +1,11 @@
+func removeElement(nums []int, val int) int {
+    var i = 0
+    for j := 0; j < len(nums); j++ {
+        if (nums[j] != val) {
+            nums[i] = nums[j]
+            i++
+        }
+    }
+
+    return i
+}
