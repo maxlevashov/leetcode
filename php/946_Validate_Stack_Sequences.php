7262ed3e2108ee374d937176f2d9b626e34d1a84
maxlevashov <maklev@mail.ru>
2023-05-08 10:40:08 +0300

    поправил названия файлов

---
+++ php/946_Validate_Stack_Sequences.php
@@ -0,0 +1,33 @@
+<?php
+
+class Solution 
+{
+
+    /**
+     * @param Integer[] $pushed
+     * @param Integer[] $popped
+     * @return Boolean
+     */
+    function validateStackSequences($pushed, $popped) 
+    {
+        $pushedCount = count($pushed);
+        $stack = new SplStack();
+
+        $j = 0;
+        foreach ($pushed as $x) {
+            $stack->push($x);
+            while (
+                !$stack->isEmpty() 
+                && $stack->top() == $popped[$j]
+            ) {
+                $stack->pop();
+                $j++;
+            }
+        }
+
+        return $stack->isEmpty();
+    }
+}
+
+
+
