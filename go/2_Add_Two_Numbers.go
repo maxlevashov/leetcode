9d600f8a6493b1805b440865e14b6667533065f5
maxlevashov <maklev@mail.ru>
2023-05-16 09:44:25 +0300

    add two numbers

--- go/2_Add_Two_Numbers.go
+++ go/2_Add_Two_Numbers.go
@@ -6,28 +6,28 @@
  * }
  */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-        dummyHead := new(ListNode)
-        curr := dummyHead
-        carry := 0
-        for l1 != nil || l2 != nil || carry != 0 {
-            var x int
-            var y int
-            if l1 != nil {
-                x = l1.Val
-                l1 = l1.Next
-            } 
-            if l2 != nil {
-                y = l2.Val
-                l2 = l2.Next
-            } 
-            
-            sum := carry + x + y
-            carry = sum / 10
-            
-            curr.Next = &ListNode{sum % 10, nil}
-            curr = curr.Next
-        }
+    dummyHead := new(ListNode)
+    curr := dummyHead
+    carry := 0
+    for l1 != nil || l2 != nil || carry != 0 {
+        var x int
+        var y int
+        if l1 != nil {
+            x = l1.Val
+            l1 = l1.Next
+        } 
+        if l2 != nil {
+            y = l2.Val
+            l2 = l2.Next
+        } 
 
-        return dummyHead.Next;
+        sum := carry + x + y
+        carry = sum / 10
+
+        curr.Next = &ListNode{sum % 10, nil}
+        curr = curr.Next
+    }
+
+    return dummyHead.Next;
 }
 
