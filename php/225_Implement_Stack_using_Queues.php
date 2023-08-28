5b95c8ef9631f2c370968ea61d97aa3cf4764fe9
maxlevashov <maklev@mail.ru>
2023-08-28 07:34:50 +0300

    225. Implement Stack using Queues

---
+++ php/225_Implement_Stack_using_Queues.php
@@ -0,0 +1,50 @@
+<?php
+
+class MyStack {
+
+    protected $queue;
+
+    /**
+     */
+    function __construct() {
+        $this->queue = new SplQueue();
+    }
+  
+    /**
+     * @param Integer $x
+     * @return NULL
+     */
+    function push($x) {
+        $this->queue->push($x);
+    }
+  
+    /**
+     * @return Integer
+     */
+    function pop() {
+        return $this->queue->pop();
+    }
+  
+    /**
+     * @return Integer
+     */
+    function top() {
+        return $this->queue->top();
+    }
+  
+    /**
+     * @return Boolean
+     */
+    function empty() {
+        return $this->queue->isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * $obj = MyStack();
+ * $obj->push($x);
+ * $ret_2 = $obj->pop();
+ * $ret_3 = $obj->top();
+ * $ret_4 = $obj->empty();
+ */
