2b89889f74adefdff8afbec07d8652908a228c29
maxlevashov <maklev@mail.ru>
2023-09-11 05:20:12 +0300

    1282. Group the People Given the Group Size They Belong To

--- php/1282_Group_the_People_Given_the_Group_Size_They_Belong_To.php
+++ php/1282_Group_the_People_Given_the_Group_Size_They_Belong_To.php
@@ -10,13 +10,13 @@ class Solution
     function groupThePeople($groupSizes) 
     {
         $result = [];
-        $szToGroup = array_fill(0, count($groupSizes) + 1, []);
+        $sizeToGroup = array_fill(0, count($groupSizes) + 1, []);
 
         for ($i = 0; $i < count($groupSizes); $i++) {
-            $szToGroup[$groupSizes[$i]][] = $i;
-            if (count($szToGroup[$groupSizes[$i]]) == $groupSizes[$i]) {
-                $result[] = $szToGroup[$groupSizes[$i]];
-                $szToGroup[$groupSizes[$i]] = [];
+            $sizeToGroup[$groupSizes[$i]][] = $i;
+            if (count($sizeToGroup[$groupSizes[$i]]) == $groupSizes[$i]) {
+                $result[] = $sizeToGroup[$groupSizes[$i]];
+                $sizeToGroup[$groupSizes[$i]] = [];
             }
         }
         
