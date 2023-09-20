b456a25e1d5fc792e55a16904282922198a48743
maxlevashov <maklev@mail.ru>
2023-09-20 07:21:22 +0300

    1658. Minimum Operations to Reduce X to Zero

--- php/1658_Minimum_Operations_to_Reduce_X_to_Zero.php
+++ php/1658_Minimum_Operations_to_Reduce_X_to_Zero.php
@@ -1,7 +1,31 @@
 <?php
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $x
+     * @return Integer
+     */
+    function minOperations($nums, $x) {
+        $sum = 0;
+        foreach ($nums as $num) {
+            $sum += $num;
+        }
+
+        $maxLength = -1;
+        $currSum = 0;
+        for ($left = 0, $right = 0; $right < count($nums); $right++) {
+            $currSum += $nums[$right];
+            while ($left <= $right && $currSum > $sum - $x) {
+                $currSum -= $nums[$left++];
+            }
+            if ($currSum == $sum - $x) {
+                $maxLength = max($maxLength, $right - $left + 1);
+            }
+        }
+
+        return $maxLength == -1 ? -1 : count($nums) - $maxLength;
+    }
+}
 
